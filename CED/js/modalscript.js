a8c703003b0c50c5165006d31a86d2b7a8173003
kalyansrinivasroy <kalyansrinivas.kodi@gmail.com>
2024-03-19 09:57:51 +0530

    committingon19.3.2024957

--- CED/js/modalscript.js
+++ CED/js/modalscript.js
@@ -86,19 +86,6 @@ window.onclick = function(event) {
 };
 
 
-function toggleDivisionQuarterField() {
-    var divisionOrQuarter = document.querySelector('input[name="division_or_quarter"]:checked').value;
-    var divisionField = document.getElementById("divisionField");
-    var quarterField = document.getElementById("quarterField");
-
-    if (divisionOrQuarter === "division") {
-        divisionField.style.display = "block";
-        quarterField.style.display = "none";
-    } else if (divisionOrQuarter === "quarter") {
-        divisionField.style.display = "none";
-        quarterField.style.display = "block";
-    }
-}
 
 function assignWorker(issueId) {
     // Open the modal for selecting a worker
--- CED/js/script.js
+++ CED/js/script.js
@@ -15,3 +15,16 @@
 
 
 // Close the modal when the user clicks on the close button
+function toggleDivisionQuarterField() {
+    var divisionOrQuarter = document.querySelector('input[name="division_or_quarter"]:checked').value;
+    var divisionField = document.getElementById("divisionField");
+    var quarterField = document.getElementById("quarterField");
+
+    if (divisionOrQuarter === "division") {
+        divisionField.style.display = "block";
+        quarterField.style.display = "none";
+    } else if (divisionOrQuarter === "quarter") {
+        divisionField.style.display = "none";
+        quarterField.style.display = "block";
+    }
+}
